0f3b3e7b231135ec3da956f74a82fe5d0e6a93c0
Kopotilov-Dmitriy <115923298+Kopotilov-Dmitriy@users.noreply.github.com>
2022-10-19 19:03:36 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+var visible1 = document.querySelector(".p_02");
+var visible2 = document.querySelector(".p_03");
+var visible3 = document.querySelector(".p_04");
+var visible4 = document.querySelector(".p_06");
+var visible5 = document.querySelector(".p_07");
+var visible6 = document.querySelector(".p_08");
+var visible7 = document.querySelector(".p_10");
+var visible8 = document.querySelector(".p_11");
+var visible9 = document.querySelector(".p_12");
+var visible10 = document.querySelector("#button_more1");
+var visible11 = document.querySelector(".trip_block1");
+var visible12 = document.querySelector("#button_more2");
+var visible13 = document.querySelector(".trip_block2");
+var visible14 = document.querySelector("#button_more3");
+var visible15 = document.querySelector(".trip_block3");
+
+function show1() {
+    visible1.style.display = "flex";
+    visible2.style.display = "flex";
+    visible3.style.display = "flex";
+    visible10.style.display = "none";
+    visible11.style.display = "flex";
+    }
+
+function show2() {
+    visible4.style.display = "flex";
+    visible5.style.display = "flex";
+    visible6.style.display = "flex";
+    visible12.style.display = "none";
+    visible13.style.display = "flex";
+    }
+
+function show3() {
+    visible7.style.display = "flex";
+    visible8.style.display = "flex";
+    visible9.style.display = "flex";
+    visible14.style.display = "none";
+    visible15.style.display = "flex";
+    }
